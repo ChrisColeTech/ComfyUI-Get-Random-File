17f2c8d46fc2a01df19ae4ac8fa8328b1756ed1c
ChrisColeTech <moneymagnet216@gmail.com>
2024-09-01 22:30:03 -0400

    added web dir

--- __init__.py
+++ __init__.py
@@ -10,3 +10,6 @@ print(f"\033[1;34m[CCTech Suite]: 🤖🤖🤖 \033[96m\033[3m{random.choice(tec
 print(f"\033[1;34m[CCTech Suite]:\033[0m Activated \033[96m{len(NODE_CLASS_MAPPINGS)}\033[0m file nodes.")
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+
+
+WEB_DIRECTORY = "./web"
